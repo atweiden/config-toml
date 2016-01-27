3dac11518fb00a9ca168ee23abb360d52a877ea5
Andy Weidenbaum <archbaum@gmail.com>
2016-01-27 15:42:13 -0800

    parse toml from file

--- lib/Config/TOML/Parser.pm
+++ lib/Config/TOML/Parser.pm
@@ -9,4 +9,10 @@ method parse(Str:D $content, Int :$date-local-offset, *%opts) returns Match
     Config::TOML::Parser::Grammar.parse($content, :$actions, |%opts);
 }
 
+method parsefile(Str:D $file, Int :$date-local-offset, *%opts) returns Match
+{
+    my Config::TOML::Parser::Actions $actions .= new(:$date-local-offset);
+    Config::TOML::Parser::Grammar.parsefile($file, :$actions, |%opts);
+}
+
 # vim: ft=perl6
